c572fae0467e3ae0cab3e346f29ea872f1d58721
BeesyBumbleBee <sz.oleskiewicz@gmail.com>
2024-06-28 19:31:07 +0200

    changed graphics

--- scripts.js
+++ scripts.js
@@ -1,50 +1,49 @@
 // set up text to print, each item in array is new line
-var aText = new Array(
+var textToWrite = new Array(
 "<div class='terminal'>> Loading user info...</div>",
 "<div class='terminal' id='user'>Name: Szymon Oleśkiewicz</div>",
-"<div class='terminal' id='user'>Profession: Software Engineer (in making)</div>",
+"<div class='terminal' id='user'>Profession: Software Engineer (in making) </div>",
+"<div class='terminal' id='contact'>Email:<a class='terminal' href='mailto:sz.oleskiewicz@gmail.com' id='email' style='display:inline'>sz.oleskiewicz@gmail.com</a></div>",
 "<div class='terminal' id='cd'>> cd </div>",
 '<a href="https://github.com/BeesyBumbleBee" class="cd-select" id="github"> .\\github\\</a>',
 '<a href="https://www.linkedin.com/in/szymon-oleskiewicz/" class="cd-select" id="linkedin">.\\linkedin\\</a>',
 '<a href="#" class="cd-select" id="resume">.\\resume\\</a>',
-'<a href="#" class="cd-select" id="contact">.\\contact\\</a>',
 '<a href="./projects/index.html" class="cd-select" id="projects">.\\projects-demo\\</a>',
 ""
 );
+ 
+var writeDelay = 25;
+var newLineDelay = 150;
+var scrollAt = 20;
+var currIndex = 0;
+var endIndex;
 
-var iSpeed = 50; // time delay of print out
-var nlSpeed = 500; //  new line delay
-var iIndex = 0; // start printing array at this posision
-var iArrLength = aText[0].length; // the length of the text array
-var iScrollAt = 20; // start scrolling up at this many lines
- 
-var iTextPos = 0; // initialise text position
-var sContents = ''; // initialise contents variable
-var iRow; // initialise current row
- 
+var currTextPos = 0;
+var contents = ' ';
+var currRow;
+
 function typewriter()
 {
-	sContents =  ' ';
-	iRow = Math.max(0, iIndex-iScrollAt);
+	contents =  ' ';
+	currRow = Math.max(0, currIndex - scrollAt);
 	var destination = document.getElementById("typedtext");
-	
-	while ( iRow < iIndex ) {
-		sContents += aText[iRow++] + '<br />';
+	if ( endIndex === undefined ) { endIndex = textToWrite.length }
+
+	while ( currRow < currIndex ) {
+		contents += textToWrite[currRow++] + '<br />';
 	}
-	destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + (iIndex+1 < iArrLength ? "_" : "");
-	if ( iTextPos++ == iArrLength ) {
-		iTextPos = 0;
-		iIndex++;
-		if ( iIndex == 4) {
-			iSpeed = 5;
-			nlSpeed = 100;
-		}
-		if ( iIndex != aText.length ) {
-			iArrLength = aText[iIndex].length;
-			setTimeout("typewriter()", nlSpeed);
+
+	destination.innerHTML = contents + textToWrite[currIndex].substring(0, currTextPos) + (currIndex+1 < endIndex ? "_" : "");
+	if ( currTextPos++ == textToWrite[currIndex].length ) {
+		currTextPos = 0;
+		currIndex++;
+		if ( currIndex != endIndex ) {
+			setTimeout("typewriter()", newLineDelay);
 		}
 	} else {
-		setTimeout("typewriter()", iSpeed);
+		if (textToWrite[currIndex][currTextPos] == '<' || currIndex >= 4) { writeDelay = 0; newLineDelay = 10 }
+		if (textToWrite[currIndex][currTextPos] == '>') { writeDelay = 25; newLineDelay= 150 }
+		setTimeout("typewriter()", writeDelay);
 	}
 	cdListeners();
 }
@@ -56,6 +55,7 @@ function cdListeners()
 {
 	const cd_sel = document.getElementsByClassName("cd-select");
 	const cd = document.getElementById("cd");
+
 	for (var i = 0; i < cd_sel.length; i++) {
 		cd_sel[i].addEventListener(
 			"mouseenter",
